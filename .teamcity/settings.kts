4640960fba4f4baad5f959729d21d65d5eba84f2
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-11-24 11:54:18 +0100

    add vcs root3

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,32 +1,12 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 version = "2023.11"
 
 project {
-
+    vcsRoot(Repo3)
+    root(Repo3, "+:. => repo3")
     buildType(Build1)
 }
 
@@ -46,5 +26,15 @@ object Build1 : BuildType({
                 echo "##teamcity[testFinished name='MyTest.test2']"
             """.trimIndent()
         }
+    }
+})
+
+object Repo3 : GitVcsRoot({
+    name = "https://github.com/ChubatovaTiger/repo3"
+    url = "https://github.com/ChubatovaTiger/repo3"
+    branch = "refs/heads/main"
+    authMethod = password {
+        userName = "ChubatovaTiger"
+        password = "credentialsJSON:a34cf740-b3bf-4a18-b387-5b54dc880bc0"
     }
 })
